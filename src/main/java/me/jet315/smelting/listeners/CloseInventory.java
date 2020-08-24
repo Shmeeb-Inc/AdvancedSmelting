698cdf27864cb11bff5136bdc3b1fe406cd80108
Chris <shiki.pers@gmail.com>
2020-08-24 15:14:59 -0400

    initial commit - added maven framework support - moved resources & classes to correct locations (based off maven) - added SpigotAPI, VaultAPI & TacoSpigot Server JAR dependencies - disabled non 1.8 items as we are using 1.8 Spigot API

---
+++ src/main/java/me/jet315/smelting/listeners/CloseInventory.java
@@ -0,0 +1,35 @@
+package me.jet315.smelting.listeners;
+
+import me.jet315.smelting.Core;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class CloseInventory implements Listener{
+
+    /**
+     * Listens to when a player closes an inventory - Not currently in use
+     */
+
+    //Check if inventory is a smelting inventory (May need to do this on quit too)
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e){
+        Inventory inventory = e.getInventory();
+        if(Core.getInstance().getSmeltingGUI().isSmeltingInventory(inventory)){
+            if(Core.getInstance().getSmeltManager().getActivelySmelting().containsKey(e.getPlayer())) return;
+            inventory.setItem(inventory.getSize()-1,null);
+            ItemStack[] contents = inventory.getContents();
+
+            for(ItemStack itemStack : contents){
+                if(itemStack == null) continue;
+                ItemStack item = new ItemStack(itemStack);
+                itemStack.setAmount(0);
+                e.getPlayer().getInventory().addItem(item);
+            }
+            Core.getInstance().getSmeltingGUI().getPlayersInGUI().remove(e.getPlayer());
+        }
+    }
+}
